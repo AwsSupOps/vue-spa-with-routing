e12b09dd990ee9d740d836675ece0f95f06696d7
EC2 Default User <ec2-user@ip-172-31-7-155.ec2.internal>
2025-07-12 10:43:16 +0000

    first commitp5

---
+++ src/views/Mypage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="Page">
+    <h1>This is my own page</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
